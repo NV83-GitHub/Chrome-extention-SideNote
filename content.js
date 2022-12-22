74000cb53b83437d5b8b7cb764db86b0945c563d
Nicolas Vandenbruaene <nicolas.vandenbruaene@gmail.com>
2022-12-22 15:30:40 +0100

    some refactoring in content.js and storage permission added in manifest

--- content.js
+++ content.js
@@ -7,26 +7,50 @@ function createBrowserNote(){
     browserNote.innerHTML = `<form>
                                 <input type="text" id="noteTitle" name="Title" placeholder="Title"><br>
                                 
-                                <textarea id="noteArea" name="Note" placeholder="Note something here">`
+                                <textarea id="noteArea" name="Note" placeholder="Note something here"></textarea>
+                                <button id="saveBtn">Save</button>`
     document.body.append(browserNote)
 }
 
-// Event received from background when user click extension icon
+// Event msg received from background when user click extension icon
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
 
         if (request.event ==="extensionBtnClicked")
         sendResponse({content_js_response : "Ok je fais le necessaire"})
 
-        toggleHidden(browserNote, "isHidden")
+        toggle(browserNote, "isHidden")
     }
 )
 
-function toggleHidden(element, className) {
+// Utility Functions 
+
+// Multi purpose Toggle function
+function toggle(element, className) {
     if (element.classList.contains(className)) {
         element.classList.remove(className)
     } else {
         element.classList.add(className)
     }
 }
+
+// Storage Logic
+
+let titleContent = document.getElementById("noteTitle")
+let noteContent = document.getElementById("noteArea")
+const saveBtn = document.getElementById("saveBtn")
+
+saveBtn.addEventListener("click", () => {
+    let key = titleContent.value 
+    let valeur = noteContent.value
+
+    chrome.storage.local.set({[key]: valeur}) // To use a variable as a key [] are needed, it's called computed property
+})
+
+
+// Features to add:
+// ----------------
+// Alert when user quit or refresh and content not saved
+// Menu to open older notes
+// when page with a note matched , show that note
 
